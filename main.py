2244120db61364a44194b79222e929b54ba00b65
yn1046 <wantedblood@2ch.hk>
2018-03-06 07:25:10 +0300

    correct encryption implemented

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 def isPrime(num):
     if num < 4:
         return True
-    for x in range(2, int(num/2)+1):
+    for x in range(2, int(num / 2) + 1):
         if num % x is 0:
             return False
     return True
 
 
 def isPrimeRoot(g, p):
-    if g**(p-1) != 1 % p:
+    if g ** (p - 1) % p != 1:
         return False
-    for L in range(1, p-2):
-        if g**L == 1 % p:
+    for L in range(1, p - 2):
+        if g ** L % p == 1:
+            return False
+    return True
+
+
+def isCoprime(first, second):
+    max = first if (first <= first) else first
+    if first == second:
+        return False
+    elif max < 4:
+        return True
+    for x in range(2, int(max / 2) + 1):
+        if first % x == 0 and second % x == 0:
             return False
     return True
 
@@ -28,17 +40,30 @@ while not isPrime(g) or not isPrimeRoot(g, p):
         g = int(input("g isn't a primitive root. Enter a primitive root: "))
 
 x = int(input("Select an (x) between 1 to p-1 (non-inclusive): "))
-while x < 2 or x > p-2:
+while x < 2 or x > p - 2:
     x = int(input("x is not in range between 1 and p-1. Enter a correct value: "))
 
-y = g**x % p
+y = g ** x % p
 print(f"y = {y}")
 
 print(f"The open key (p, g, y) is ({p}, {g}, {y})")
 print(f"The closed key K(c) is {x}")
 
 m = int(input("Enter a message digit in range 0 to p-1: "))
-while m < 0 or m > p-1:
+while m < 0 or m > p - 1:
     m = int(input("The message wasn't in range 0 to p-1. Enter a correct value: "))
 
+k = int(input("Enter (k) between 1 to p-1 (non-inclusive), coprime to p-1: "))
+while k < 2 or k > p - 2 or not isCoprime(k, p - 1):
+    if k < 2 or k > p - 2:
+        k = int(input("k was not in range (1, p-1). Enter a correct value: "))
+    else:
+        k = int(input("k is not coprime to p-1. Enter a correct value: "))
 
+a = g ** k % p
+b = (y ** k) * m % p
+
+print(f"Cipher-text: ({a}, {b})")
+
+solution = (b * (a ** x) ** (p - 2)) % p
+print(f"Decrypted text: {solution}")
